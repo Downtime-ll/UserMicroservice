0cbba308fe7e7eff4791d23b4693f9c5da89bb60
BoyceLyu <lvzhendong.bl@gmail.com>
2016-04-14 16:36:46 +0800

    创建项目

---
+++ UserMicroservices/src/User.Domain/IUser.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace User.Domain
+{
+    /// <summary>
+    /// 用户信息类
+    /// </summary>
+    public interface IUser
+    {
+
+    }
+}
---
+++ UserMicroservices/src/User.Domain/Properties/AssemblyInfo.cs
@@ -0,0 +1,23 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("User.Domain")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("User.Domain")]
+[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("33a9f858-4da1-4855-8451-0b24f9c9a203")]
